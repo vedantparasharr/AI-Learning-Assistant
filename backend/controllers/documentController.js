0993ea6342340b24c8cc6f786f6e5cbab26e5ed2
vedantparasharr <iemvedant@gmail.com>
2026-02-28 23:23:22 +0530

    feat: complete document upload and documents retrieval functionality with error handling and PDF processing

--- backend/controllers/documentController.js
+++ backend/controllers/documentController.js
@@ -1,8 +1,9 @@
 import User from "../models/User.js";
 import Flashcard from "../models/Flashcard.js";
 import Quiz from "../models/Quiz.js";
-// import { extractTextFromPDF } from "../utils/pdfParser.js";
-// import { chunkText } from "../utils/textChunker.js";
+import Document from "../models/Document.js";
+import { extractTextFromPDF } from "../utils/pdfParser.js";
+import { chunkText } from "../utils/textChunker.js";
 import fs from "fs/promises";
 import mongoose from "mongoose";
 
@@ -11,7 +12,76 @@ import mongoose from "mongoose";
 // access Private
 export const uploadDocument = async (req, res, next) => {
   try {
-  } catch {}
+    if (!req.file) {
+      return res.status(400).json({
+        success: false,
+        error: "Please upload a PDF File",
+        statusCode: 400,
+      });
+    }
+
+    const { title } = req.body;
+    if (!title) {
+      // Delete if title does not exist
+      await fs.unlink(req.file.path);
+      return res.status(400).json({
+        success: false,
+        error: "Please provide a document title",
+        statusCode: 400,
+      });
+    }
+
+    // Construct the URL for the uploaded file
+    const baseUrl = `http://localhost:${process.env.PORT || 8000}`;
+    const fileUrl = `${baseUrl}/uploads/documents/${req.file.filename}`;
+
+    // Create document record
+    const document = await Document.create({
+      userId: req.user.id,
+      title,
+      fileName: req.file.originalname,
+      filePath: fileUrl,
+      fileSize: req.file.size,
+      status: "processing",
+    });
+
+    processPDF(document._id, req.file.path).catch((err) => {
+      console.error("PDF Processing error", err);
+    });
+
+    res.status(201).json({
+      success: true,
+      data: document,
+      message: "Document Uploaded. Processing...",
+    });
+  } catch (error) {
+    if (req.file) {
+      await fs.unlink(req.file.path).catch(() => {});
+    }
+    next(error);
+  }
+};
+
+// Helper function to process PDF
+const processPDF = async (documentId, filePath) => {
+  try {
+    const { text } = await extractTextFromPDF(filePath);
+    const chunks = await chunkText(text, 500, 50);
+
+    // Update document
+    await Document.findByIdAndUpdate(documentId, {
+      extractedText: text,
+      chunks: chunks,
+      status: "ready",
+    });
+    console.log(`Document ${documentId} processed successfully`);
+  } catch (error) {
+    console.error(`Error processing document ${documentId}`, error);
+
+    await Document.findByIdAndUpdate(documentId, {
+      status: "failed",
+    });
+  }
 };
 
 // @desc Get all user documents
@@ -19,10 +89,53 @@ export const uploadDocument = async (req, res, next) => {
 // access Private
 export const getDocuments = async (req, res, next) => {
   try {
+    const documents = await Document.aggregate([
+      {
+        $match: { userId: new mongoose.Types.ObjectId(req.user._id) },
+      },
+      {
+        $lookup: {
+          from: "flashcards",
+          localField: "_id",
+          foreignField: "documentId",
+          as: "flashcardSets",
+        },
+      },
+      {
+        $lookup: {
+          from: "quizzes",
+          localField: "_id",
+          foreignField: "documentId",
+          as: "quizzes",
+        },
+      },
+
+      {
+        $addFields: {
+          flashcardCount: { $size: "$flashcardSets" },
+          quizCount: { $size: "$quizzes" },
+        },
+      },
+      {
+        $project: {
+          extractedText: 0,
+          chunks: 0,
+          flashCardSets: 0,
+          quizzes: 0,
+        },
+      },
+      {
+        $sort: {
+          uploadDate: -1,
+        },
+      },
+    ]);
+    res.status(200).json({
+      success: true,
+      count: documents.length,
+      data: documents,
+    });
   } catch (error) {
-    if (req.file) {
-      await fs.unlink(req.file.path).catch(() => {});
-    }
     next(error);
   }
 };
