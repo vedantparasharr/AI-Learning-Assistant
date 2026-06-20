f336d31f7678000e5ca9f33f4c8fac980e72fc2f
vedantparasharr <iemvedant@gmail.com>
2026-06-20 22:55:32 +0530

    chore: update mongoose and mongodb dependencies and their internal library files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+import dns from "dns";
 
 const connectDB = async () => {
   try {
+    // Set DNS servers to Google and Cloudflare to resolve SRV querySrv issues
+    dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
     const conn = await mongoose.connect(process.env.MONGODB_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
+    console.log(process.env.MONGODB_URL);
     console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
   }
